23023fbdd81f59997228106c0a457a5aff111944
taioo <t.aldefai@outlook.com>
2019-09-12 21:28:31 +0200

    edit security

--- Spring/src/main/java/myApp/WebSecurityConfig.java
+++ Spring/src/main/java/myApp/WebSecurityConfig.java
@@ -14,20 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
+
 
     @Bean
     @Override
@@ -36,9 +23,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
              User.withDefaultPasswordEncoder()
                 .username("user")
                 .password("password")
-                .roles("USER")
+                .roles("admin")
                 .build();
 
         return new InMemoryUserDetailsManager(user);
     }
+
+    
 }
