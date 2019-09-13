b23be8fa3780b73993a6c1dd7f89c793416a22c3
taioo <t.aldefai@outlook.com>
2019-09-13 14:51:54 +0200

    add postman

--- Spring/src/main/java/myApp/WebSecurityConfig.java
+++ Spring/src/main/java/myApp/WebSecurityConfig.java
@@ -3,12 +3,15 @@ package myApp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -16,18 +19,18 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-             User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("admin")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	    auth.inMemoryAuthentication()
+	        .withUser("admin").password(encoder().encode("admin")).roles("ADMIN")
+	        .and()
+	        .withUser("user").password(encoder().encode("password")).roles("USER");
+	}
+	 
+	@Bean
+	public PasswordEncoder  encoder() {
+	    return new BCryptPasswordEncoder();
+	}
 
     
 }
