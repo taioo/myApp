e61004cd22923ab4c4854f58e4494ade4c683177
taioo <t.aldefai@outlook.com>
2019-09-14 17:50:53 +0200

    edit user & usercontroller

--- Spring/src/main/java/myApp/controller/UserController.java
+++ Spring/src/main/java/myApp/controller/UserController.java
@@ -20,13 +20,11 @@ public class UserController {
 
 	@PostMapping(path="/add") // Map ONLY POST Requests
 	public @ResponseBody String addNewUser (@RequestParam String name
-			, @RequestParam String email) {
+			, @RequestParam String email , @RequestParam String password) {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
 
-		User n = new User();
-		n.setName(name);
-		n.setEmail(email);
+		User n = new User(name, email ,password);
 		userRepository.save(n);
 		return "Saved";
 	}
--- Spring/src/main/java/myApp/model/User.java
+++ Spring/src/main/java/myApp/model/User.java
@@ -1,43 +1,83 @@
 package myApp.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity
+@Table(name = "users")
 public class User {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
 
     private String name;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private boolean enabled = true;
+    private Date lastLogin;
 
-    private String email;
+    private User() {
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public User(String name, String email, String password) {
+        this.username = email;
+        this.name = name;
+        this.password = password;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
 }
